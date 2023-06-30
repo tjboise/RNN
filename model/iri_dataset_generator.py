85e98db2372fee3422fced66825628964ea91927
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2023-06-30 16:46:45 -0600

    Update iri_dataset_generator.py
    Fixed boundary scaling issue caused by not mean-centering and normalizing the classification bounds

--- model/iri_dataset_generator.py
+++ model/iri_dataset_generator.py
@@ -1,4 +1,5 @@
 from typing import Tuple
+import numpy as np
 from tqdm import tqdm
 from torch.utils.data import Dataset
 import torch
@@ -66,29 +67,28 @@ class IRIBucketDataset(IRIDataset):
         # boundries[1] <= poor
         boundries = [1.5, 2.68]
         target = [0] * 3 # good, acceptable, poor one-hot encoded
-        if mean < boundries[0]:
+        if mean < (boundries[0] - mean_iri) / iri_range:
             target[0] = 1
-        elif mean < boundries[1]:
+        elif mean < (boundries[1] - mean_iri) / iri_range:
             target[1] = 1
         else:
             target[2] = 1
         return torch.from_numpy(self._inputs[idx].T).float(), torch.tensor(target).float()
 
-def normalize_columns(df: pd.DataFrame, columns: list) -> pd.DataFrame:
+def normalize_columns(df: pd.DataFrame, columns: list, iri_range: float) -> pd.DataFrame:
     """
     Normalizes the given columns in the given dataframe to (-1, 1)
     """
     for col in columns:
-        df[col] = (df[col] - df[col].min()) / (max(df[col]) - min(df[col]))
+        df[col] = (df[col] - df[col].min()) / iri_range
     return df
 
-def mean_center_columns(df: pd.DataFrame, columns: list) -> pd.DataFrame:
+def mean_center_columns(df: pd.DataFrame, columns: list, mean_iri: float) -> pd.DataFrame:
     """
     Mean centers the given columns in the given dataframe.
     """
-    global_mean = df[columns].mean().mean()
     for col in columns:
-        df[col] = df[col] - global_mean
+        df[col] = df[col] - mean_iri
     return df
 
 def load_iri_datasets(seed=42,
@@ -116,12 +116,12 @@ def load_iri_datasets(seed=42,
     mean_iri = (sorted_data["IRI_LEFT_WHEEL_PATH"].mean() + sorted_data["IRI_RIGHT_WHEEL_PATH"].mean()) / 2
 
 
-    sorted_data = mean_center_columns(sorted_data, ["IRI_LEFT_WHEEL_PATH", "IRI_RIGHT_WHEEL_PATH"])
+    sorted_data = mean_center_columns(sorted_data, ["IRI_LEFT_WHEEL_PATH", "IRI_RIGHT_WHEEL_PATH"], mean_iri)
 
     iri_range = max(sorted_data["IRI_LEFT_WHEEL_PATH"].max() - sorted_data["IRI_LEFT_WHEEL_PATH"].min(),
                     sorted_data["IRI_RIGHT_WHEEL_PATH"].max() - sorted_data["IRI_RIGHT_WHEEL_PATH"].min())
     
-    sorted_data = normalize_columns(sorted_data, ["IRI_LEFT_WHEEL_PATH", "IRI_RIGHT_WHEEL_PATH"])
+    sorted_data = normalize_columns(sorted_data, ["IRI_LEFT_WHEEL_PATH", "IRI_RIGHT_WHEEL_PATH"], iri_range)
 
     # Split into train and test
     ids = sorted_data[["SHRP_ID", "STATE_CODE"]].drop_duplicates()
@@ -139,10 +139,3 @@ def load_iri_datasets(seed=42,
         train_dataset = IRIDataset(train_data, seq_length)
         test_dataset = IRIDataset(test_data, seq_length)
     return (train_dataset, test_dataset)
-
-
-if __name__ == "__main__":
-    first, _ = load_iri_datasets(one_hot=True)
-    for i in range(10):
-        print(first[i])
-    print("\n\n")
